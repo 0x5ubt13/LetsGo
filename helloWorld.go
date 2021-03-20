37708671750406986814756ab3596c2885ee9980
5ubt13 <5ubt13@protonmail.com>
2021-03-20 23:52:30 +0000

    sync

--- helloWorld.go
+++ helloWorld.go
@@ -104,9 +104,9 @@ func primitives() {
 	fmt.Println(a % ^b) // 0100
 
 	// Bit shifting
-	a := 8              // 2^3
-	fmt.Println(a << 3) //  2^3 * 2^3 = 2^6
-	fmt.Println(a >> 3) //  2^3 / 2^3 = 2^0
+	x := 8              //  2^3
+	fmt.Println(x << 3) //  2^3 * 2^3 = 2^6
+	fmt.Println(x >> 3) //  2^3 / 2^3 = 2^0
 
 	// Floating point literals
 
@@ -114,11 +114,12 @@ func primitives() {
 	// float32 -> from +-1.18e-38 up to +-3.4e38
 	// float64 -> from +-2.23e-308 up to +-1.8e308
 
-	var n float32 = 3.14
-	// n = 13.7e72 <-- doesn't work with float32
-	var n float64 = 13.7e72
-	n = 2.1e14
-	fmt.Printf("%v, %T\n", n, n)
+	// var y float32 = 3.14
+	// y = 13.7e72 <-- doesn't work with float32
+	var y float64 = 13.7e72
+	z := 2.1e14
+	fmt.Printf("%v, %T\n", y, y)
+	fmt.Printf("%v, %T\n", z, z)
 
 	// Reminder: float32 and float64 don't compute, both must be same type.
 	//           no modulus              |
@@ -132,22 +133,22 @@ func primitives() {
 	// complex64
 	// Same operations available as float32 & 64
 
-	var n complex64 = 1 + 2i
-	fmt.Printf("%v, %T\n", n, n)
-	fmt.Printf("%v, %T\n", real(n), real(n))
-	fmt.Printf("%v, %T\n", imag(n), imag(n))
+	var u complex64 = 1 + 2i
+	fmt.Printf("%v, %T\n", u, u)
+	fmt.Printf("%v, %T\n", real(u), real(u))
+	fmt.Printf("%v, %T\n", imag(u), imag(u))
 
 	// Putting together a real number and an imaginary number to make a complex number
-	var n complex128 = complex(5, 12)
-	fmt.Printf("%v, %T\n", n, n) // output = (5+12i), complex128
+	var t complex128 = complex(5, 12)
+	fmt.Printf("%v, %T\n", t, t) // output = (5+12i), complex128
 
 	// Strings
 	// In Go, strings are aliases for UTF-8 encoded bytes
 	s := "this is a string"
 	//s[2] = "u" // error, cannot use "u" as type byte in assignment
-	b = []byte(s)                               // will print all bytes corresponding to each of the letters
-	fmt.Println("%v, %T\n", s[2], s[2])         // output = 105, uint8
-	fmt.Println("%v, %T\n", string(s[2]), s[2]) // output = i, uint8
+	f := []byte(s)                             // will print all bytes corresponding to each of the letters
+	fmt.Printf("%v, %T\n", f[2], f[2])         // output = 105, uint8
+	fmt.Printf("%v, %T\n", string(f[2]), f[2]) // output = i, uint8
 
 	// Runes
 	// Like strings but UTF-32 instead, alias for int32
@@ -157,7 +158,67 @@ func primitives() {
 	// For reading off of a rune, watch https://golang.org/pkg/strings/#Reader.ReadRune
 }
 
+func constants() {
+
+	// Constants:
+
+	// Very similar concept than vars, they can be shadowed and acquire same types however they are immutable
+	// Interesting feature is IOTA:
+	// 	  - Naming constants and assigning iota to the first constant in a constants block
+	// 		keeps track of the occurrences of every each one of them like a counter but we
+	//		can assign a special value to that counter. Examples:
+
+	// iota example 1:
+	const (
+		_ = iota + 5
+		catSpecialist
+		dogSpecialist
+		snakeSpecialist
+	)
+
+	fmt.Printf("%v\n", snakeSpecialist) // output = 8
+
+	// iota example 2:
+	type ByteSize float64
+	const (
+		_  = iota // ignore first value by assigning to blank identifier
+		KB = 1 << (10 * iota)
+		MB
+		GB
+		TB
+		PB
+		EB
+		ZB
+		YB
+	)
+
+	fileSize := 4000000000.
+	fmt.Printf("%.2fGB\n", fileSize/GB)
+
+	// Setting boolean flags inside of a single byte with bit shifting and iota
+
+	const (
+		isAdmin = 1 << iota
+		isHeadquarters
+		canSeeFinancials
+
+		canSeeAfrica
+		canSeeAsia
+		canSeeEurope
+		canSeeNorthAmerica
+		canSeeSouthAmerica
+	)
+
+	var roles byte = isAdmin | canSeeFinancials | canSeeAsia | canSeeSouthAmerica
+	fmt.Printf("%b\n", roles)
+	fmt.Printf("Is Admin? %v\n", isAdmin&roles == isAdmin)
+	fmt.Printf("Is Headquarters? %v\n", isHeadquarters&roles == isHeadquarters)
+	fmt.Printf("Can see Europe? %v\n", canSeeEurope&roles == canSeeEurope)
+
+}
+
 func main() {
 	basicVariables()
 	primitives()
+	constants()
 }
