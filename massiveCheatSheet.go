9d28a64f3e8ed812cb3d9be7d527db3edf3ab721
5ubt13 <5ubt13@protonmail.com>
2021-03-28 12:51:47 +0100

    Maps and Structs finished

--- massiveCheatSheet.go
+++ massiveCheatSheet.go
@@ -541,10 +541,30 @@ func mapsAndStructs() {
 		Origin string
 	}
 
-	// Import reflection package (reflect)
+	// Import reflection package (reflect) to check Tags
 	embedType := reflect.TypeOf(NewAnimal{})
 	field, _ := embedType.FieldByName("Name")
 	fmt.Println(field.Tag)
+
+	// Summary:
+	//
+	// - Maps
+	// 		> Collections of value types that are accessed via keys
+	//		> Created via literals or make function
+	//		> Members accessed via [key] syntax
+	//			* myMap["key"] = "value"
+	//		> Check for presence with "value, ok" form of result
+	//		> Multiple assignments refer to same underlying data
+	//
+	//
+	// - Structs
+	//		> Collection of disparate data types that describe a single concept
+	//		> Keyed by named fields
+	// 		> Normally created as types, but anonymous structs are allowed
+	// 		> Structs are value types
+	//		> No inheritance, but can use composition via embedding
+	//		> Tags can be added to struct fields to describe field
+
 }
 
 func main() {
