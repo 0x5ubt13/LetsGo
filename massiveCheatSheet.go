5158cfe6870ea7fb4ebf2f9e3d4b0704cb686bc7
5ubt13 <5ubt13@protonmail.com>
2021-04-20 11:19:26 +0100

    adding interfaces

--- massiveCheatSheet.go
+++ massiveCheatSheet.go
@@ -2,7 +2,7 @@ package main // Go scripts always start with this line, either main or something
 
 import (
 	"fmt" // "format" - always import this to printing out
-	"go/ast"
+	//"go/ast"
 	"io/ioutil" // "input outout utility" - to read the body of a request
 	"log"       // to log errors
 	"math"
@@ -1141,16 +1141,16 @@ func functionsMasterclass() {
 	basics()
 	variatics()
 	multipleReturns()
-	mrdivide()
+	_, _ = mrdivide(5.0, 2.5)
 
 	// Immediately invoked anonymous function
 	func() {
-		msg := "Immediately invoked function"
+		msg := "This is an immediately invoked function"
 		fmt.Println(msg)
 	}()
 	// Declared variable func
 	f := func() {
-		fmt.Println("Declared variable function")
+		fmt.Println("This is a declared variable function")
 	}
 	f()
 
@@ -1170,6 +1170,100 @@ func functionsMasterclass() {
 	}
 	fmt.Println(fd)
 
+	// Methods declaration (cut & paste this snippet into the main func)
+	g := greeter{
+		greeting: "Heya",
+		name:     "Golang",
+	}
+	g.greet()
+	fmt.Println("The new name is:", g.name)
+
+	// Functions summary
+	//		- Basic syntax (if name is capitalised, function will be able to be exported
+	//			> func foo() {
+	//				...
+	//			  }
+	//
+	//		- Parameters
+	//			> Comma delimited list of variables and types
+	//				· func foo(bar string, baz int)
+	//			> Parameters of same type list type once
+	//				· func foo(bar, baz int)
+	//			> When pointers are passed in, the function can change the value in the caller
+	//				· This is always true for data of slices and maps
+	//			> Use variadic parameters to send list of same types in
+	//				· Must use last parameter
+	//				· Received as a slice
+	//				· func foo (bar string, baz ...int)
+	//
+	//		- Return values
+	//			> Single return values just list type
+	//				· func foo() int
+	//			> Multiple return value list types surrounded by parentheses
+	//				· func foo() (int, error)
+	//				· The (result type, error) paradigm is a very common idiom
+	//			> Can use named return values
+	//				· Initialises returned variable
+	//				· Return using return keyword on its own
+	//			> Can return addresses of local variables
+	//				· Automatically promoted from local memory (stack) to shared memory (heap)
+	//
+	//		- Anonymous functions
+	//			> Functions don't have names if they are:
+	//				· Immediately invoked
+	//					¬ func() {
+	//						...
+	//					  }()
+	//				· Assigned to a variable or passed as an argument to a function
+	//					¬ a := func() {
+	//						...
+	//					  }
+	//					  a()
+	//
+	//		- Functions as types
+	//			> Can assign functions to variables or use as arguments and return values in functions
+	//			> Type signature is like function signature, with no parameter names. First parentheses are types coming
+	//	  		  in, and second parenthesis are types coming out
+	//				· var f func(string, string, int) (int, error)
+	//
+	//		- Methods
+	//			> Function that executes in context of a type
+	//			> Format:
+	//				· func (g greeter) greet() {
+	//					...
+	//				  }
+	//			> Receiver can be either value or pointer
+	//				· Value receiver gets copy of type
+	//				· Pointer receiver gets pointer to type
+}
+
+// Methods declaration
+type greeter struct {
+	greeting string
+	name     string
+}
+
+func (g *greeter) greet() {
+	fmt.Println(g.greeting, g.name)
+	g.name = "pointer to greeter function!"
+}
+
+//////////////////
+/// Interfaces ///
+//////////////////
+
+func interfaces() {
+	// Basics
+
+	// Composing interfaces
+
+	// Type conversion
+	// The empty interface
+	// Type switches
+
+	// Implementing with values vs pointers
+
+	// Best practices
 }
 
 func main() {
@@ -1188,5 +1282,6 @@ func main() {
 	//panicking()
 	//recovering()
 	//pointers()
-	functionsMasterclass()
+	//functionsMasterclass()
+	interfaces()
 }
