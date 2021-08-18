7feeb53b723c1fa9b3aab9c9b044afbf5306932e
0x5ubt13 <5ubt13@protonmail.com>
2021-08-18 22:26:08 +0100

    create new file

---
+++ goPortScanner.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/sync/semaphore"
+)
+
+type PortScanner struct {
+	ip   string
+	lock *semaphore.Weighted
+}
