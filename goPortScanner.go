fa39911b7fe48db1ec421560a33fa8de2796d896
0x5ubt13 <5ubt13@protonmail.com>
2021-08-21 10:07:39 +0100

    sync

--- goPortScanner.go
+++ goPortScanner.go
@@ -1,19 +1,56 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"golang.org/x/sync/semaphore"
 	"net"
 	"os/exec"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
-
-	"golang.org/x/sync/semaphore"
 )
 
 type PortScanner struct {
-	ip   string
+	// The host to scan
+	ip string
+
+	// The threshold to limit the goroutines
 	lock *semaphore.Weighted
+}
+
+func Ulimit() int64 {
+	// Call the built-in command `ulimit` to help lock
+	out, err := exec.Command("ulimit", "-n").Output()
+	if err != nil {
+		panic(err)
+	}
+
+	s := strings.TrimSpace(string(out))
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+func ScanPort(ip string, port int, timeout time.Duration) {
+	target := fmt.Sprintf("%s:%d", ip, port)
+
+	conn, err := net.DialTimeout("tcp", target, timeout)
+	if err != nil {
+		// Handling socket error
+		if strings.Contains(err.Error(), "too many open files") {
+			time.Sleep(timeout)
+			ScanPort(ip, port, timeout)
+			//
+		} else {
+			fmt.Println(port, "closed")
+		}
+
+		return
+	}
+
+	conn.Close()
+	fmt.Println(port, "open")
 }
