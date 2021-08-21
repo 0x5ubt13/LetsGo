06a52f83d3832ca6b562de125e18bc070a470aca
0x5ubt13 <5ubt13@protonmail.com>
2021-08-21 11:54:39 +0100

    sync

--- goPortScanner.go
+++ goPortScanner.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"golang.org/x/sync/semaphore"
 	"net"
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -43,7 +45,7 @@ func ScanPort(ip string, port int, timeout time.Duration) {
 		if strings.Contains(err.Error(), "too many open files") {
 			time.Sleep(timeout)
 			ScanPort(ip, port, timeout)
-			//
+			// Handling closed port
 		} else {
 			fmt.Println(port, "closed")
 		}
@@ -53,4 +55,29 @@ func ScanPort(ip string, port int, timeout time.Duration) {
 
 	conn.Close()
 	fmt.Println(port, "open")
+}
+
+func (ps *PortScanner) Start(f, l int, timeout time.Duration) {
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	for port := f; port <= l; port++ {
+		wg.Add(1)
+		ps.lock.Acquire(context.TODO(), 1)
+
+		go func(port int) {
+			defer ps.lock.Release(1)
+			defer wg.Done()
+			ScanPort(ps.ip, port, timeout)
+		}(port)
+	}
+}
+
+func main() {
+	ps := &PortScanner{
+		ip:   "127.0.0.1",
+		lock: semaphore.NewWeighted(Ulimit()),
+	}
+
+	ps.Start(1, 65535, 500*time.Millisecond)
 }
