76de9c97c62edf53cb70562fdee67fc4e34cce92
0x5ubt13 <5ubt13@protonmail.com>
2021-08-22 13:16:32 +0100

    working

--- Gophercises/1 - Quiz game/quiz_game.go
+++ Gophercises/1 - Quiz game/quiz_game.go
@@ -5,10 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a .CSV file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "The time limit for the quiz in seconds")
 	flag.Parse()
 
 	f, err := os.Open(*csvFilename)
@@ -18,27 +21,49 @@ func main() {
 	}
 	defer f.Close()
 
+	// read the input file
 	r := csv.NewReader(f)
 	lines, err := r.ReadAll()
 	if err != nil {
 		exit("Failed to parse the provided .CSV file.")
 	}
+
 	problems := parseLines(lines)
+	correct := 0
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
-	// Main control flow
+	// Main quiz control flow
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-	}
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
 
+		// Creating an anonymous func and a channel to handle the user input
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nTime's out! You scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.a {
+				fmt.Println("Correct!")
+				correct++
+			}
+		}
+	}
 }
 
+// Parse the input file
 func parseLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 
 	for i, line := range lines {
 		ret[i] = problem{
-			q: line[0],
-			a: line[1],
+			q: strings.TrimSpace(line[0]),
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
@@ -49,6 +74,7 @@ type problem struct {
 	a string
 }
 
+// Predeclaring exit triggers
 func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
