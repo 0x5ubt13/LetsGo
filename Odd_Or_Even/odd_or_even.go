b7b07c32a4c11bfd64f2367560f3aa73a312c082
Gabriel Garcia <5ubt13@protonmail.com>
2021-09-19 20:52:36 +0100

    sync

--- Odd_Or_Even/odd_or_even.go
+++ Odd_Or_Even/odd_or_even.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	intSlice := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for i := range intSlice {
-		if i%2 == 0 {
-			fmt.Printf("%d is even\n", i)
+	for _, d := range intSlice {
+		if d % 2 == 0 {
+			fmt.Printf("%d is even\n", d)
 		} else {
-			fmt.Printf("%d is odd\n", i)
+			fmt.Printf("%d is odd\n", d)
 		}
 	}
 }
