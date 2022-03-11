7b8776902131f98d9cee68413d85feadfad11807
0x5ubt13 <5ubt13@protonmail.com>
2022-03-11 17:39:15 +0000

    tweaking the exercise a bit

--- Calgolator/main.go
+++ Calgolator/main.go
@@ -6,26 +6,39 @@ import (
 	"strings"
 	"strconv"
 	"os"
+	"math"
 )
 
 func main() {
+	// Creating the reader buffer so we can use it afterwards 
 	reader := bufio.NewReader(os.Stdin)
+
+	// First number
 	fmt.Print("Enter the first number: ")
 	aInput, _ := reader.ReadString('\n')
 	a, err := strconv.ParseFloat(strings.TrimSpace(aInput), 64)
 	if err != nil {
 		panic(err)
 	} 
+
+	// Second number
 	fmt.Print("Enter the second number: ")
 	bInput, _ := reader.ReadString('\n')
 	b, err := strconv.ParseFloat(strings.TrimSpace(bInput), 64)
 	if err != nil {
 		panic(err)
 	}
+
+	// Now that the numbers are parsed, add them up calling adding()
 	adding(a, b)
 }
 
 func adding(a float64, b float64) {
+	// Sum the values passed 
 	c := a + b
-	fmt.Printf("The sum of %v and %v equals %v\n", a, b, c)
+	
+	// Making sure we have a precise decimal number
+	d := math.Round(c*100) / 100
+
+	fmt.Printf("The sum of %v and %v equals %v\n", a, b, d)
 }
