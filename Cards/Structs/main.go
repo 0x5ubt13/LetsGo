de22c0b388a065061183d5783b8efb483c2bf770
0x5ubt13 <5ubt13@protonmail.com>
2022-03-20 14:55:15 +0000

    sync

--- Cards/Structs/main.go
+++ Cards/Structs/main.go
@@ -1,13 +1,45 @@
 package main
 
-import("fmt")
+import (
+	"fmt"
+)
 
-func main() {
-	gab := person{firstName: "Gab", lastName: "Gar"}
-	fmt.Println(gab)
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+type contactInfo struct {
+	email string
+	zipCode int
 }
 
 type person struct {
 	firstName string
 	lastName string
+	hackerAlias string
+	contact contactInfo // embedded struct
+}
+
+func main() {
+	var zc person
+	zc.firstName = "Dade"
+	zc.lastName = "Murphy"
+	zc.hackerAlias = "Zero Cool"
+	
+	mr := person{firstName: "Elliot", lastName: "Alderson"}
+	mr.hackerAlias = "Mr. Robot"
+
+	n := person{
+		firstName: "Thomas",
+		lastName: "Anderson",
+		hackerAlias: "Neo",
+		contact: contactInfo{
+			email: "thechosenone@matrix.com",
+			zipCode: 99999,
+		},
+	}
+
+	fmt.Printf("%+v\n", zc)
+	fmt.Println(mr)
+	n.print()
 }
