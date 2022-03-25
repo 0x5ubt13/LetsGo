f72ed54d4d0a13b737085447ce3fd17fc68602f6
0x5ubt13 <5ubt13@protonmail.com>
2022-03-25 07:49:38 +0000

    starting concurrency

---
+++ Basic concepts/Concurrency/Channels/secuentialMain.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	websites := []string {
+		"https://google.com",
+		"https://stackoverflow.com",
+		"https://facebook.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
+
+	for _, site := range websites {
+		code := checkSiteStatus(site)
+		fmt.Printf("Site: %v\tStatus code: %v\n", site[8:], code)
+	}
+}
+
+func checkSiteStatus(site string) int {
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Site ", site, "returned an error: ", err)
+		return 1
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode
+}
